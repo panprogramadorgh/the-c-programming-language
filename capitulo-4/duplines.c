c8c70955523efb1896bfae91040f24a256481261
panprogramadorgh <barreroalvaro2007@gmail.com>
2024-09-14 20:20:23 +0200

    Version mejorada de duplines.

--- capitulo-4/duplines.c
+++ capitulo-4/duplines.c
@@ -1,114 +1,71 @@
 #include <stdio.h>
 
 /* Determines the maximum length for input lines. */
-#define MAXLINE 1024
+#define MAXLINE (1U << 10)
 
 /* Determines the maximum amount of lines to be analized. */
-#define MAXLINES 128
+#define MAXLINES (1U << 7)
 
-/* Allows to get a line of text from the stdinput and returns the length of the string literal introduced. */
-unsigned int ginput(char line[], unsigned int limit);
+/* Compruba si la linea actual esta duplicada comprobando cada una de las empezando desde la actual hasta la primera. */
+int chcurline(const unsigned int curline, const unsigned int limit);
 
-/* Divides `s` into different lines inside `lines`. */
-int split_lines(const char s[], char lines[][MAXLINE], unsigned int mlns);
+/* Almacena todas las lineas de entrada. */
+char lines[MAXLINES][MAXLINE];
 
-/* Returns the position of `t` inside `s` starting from `beg`. */
-int strindex(const char s[], const char t[], unsigned int beg);
-
-/* Returns the lenght of `s`. */
-int strlength(const char s[]);
-
-/*
-FIXME: Programa mal planetado.
-
-Es totalmente preferible manejar la logica del programa al mismo tiempo que se van introduciendo nuevas lineas. La razon de ello es que el programa se vuelve incrementalmente mas complicado manejar los limites de maxima cantidad de linas asi como de maxima cantida de caracteres por linea.
-En estos momentos `strindex` esta buscando, y encontrando con exito en ocasiones, secuencias de caracteres fuera del limite de caracteres por linea asi como de cantidad de linas.
-Fuera de eso la logica es limpia y esta bien manejada.
-
-*/
-
-/* Programa para verificar si existen lineas duplicadas. */
+/* Programa para verificar la existencia de lineas duplicadas. */
 int main()
 {
-  char text[MAXLINE];
-  char lines[MAXLINES][MAXLINE];
-  /* Relacinado con el manejo de ocurrencias. */
-  int pos, ocu;
-  /* Relacionado con el manejo de las lineas. */
-  int l, lnlen, lns;
+  /* caracter, linea actual, indice dentro de linea, salir. */
+  int c, line, cindex, out = 0;
 
-  ginput(text, MAXLINE);                    // Extrae la entrada.
-  lns = split_lines(text, lines, MAXLINES); // Extrae lineas de la entrada.
-
-  for (l = 0; l < lns; l++)
+  for (line = 0; !out && line < MAXLINES; line++)
   {
-    lnlen = strlength(lines[l]);
-
-    for (ocu = 0, pos = -1; (ocu < 2) && (pos = strindex(text, lines[l], pos + 1)) >= 0;)
-      if ((text[pos + lnlen] == '\n') || (text[pos + lnlen] == '\0'))
-        ocu++;
-
-    if (ocu > 1)
+    for (cindex = 0;
+         !out &&
+         (c = getchar()) != '\n';
+         cindex++)
     {
-      printf("%s\n", lines[l]); // La linea duplicada se imprime
-      goto error;
+      if (c == EOF || cindex >= MAXLINE - 1)
+      {
+        out = 1;
+        continue;
+      }
+      lines[line][cindex] = c;
+    }
+    lines[line][cindex] = '\0';
+    if (chcurline(line, MAXLINES))
+    {
+      printf("%s\n", lines[line]);
+      return 0;
     }
   }
-  return 0;
 
-error:
+  if (out)
+  {
+    fprintf(stderr, "Program exit caused for an EOF character or line overflow.\n");
+  }
+  else
+  {
+    fprintf(stderr, "Program exit caused for max amount of lines reached.\n");
+  }
+
   return 1;
 }
 
-unsigned int ginput(char line[], unsigned int limit)
+int chcurline(const unsigned int curline, const unsigned int limit)
 {
-  int i, c;
-  for (i = 0; i < limit - 1 && (c = getchar()) != EOF; i++)
-    line[i] = c;
-  line[i] = '\0';
-  return i;
-}
+  int line, i;
 
-int split_lines(const char s[], char lines[][MAXLINE], unsigned int mlns)
-{
-  int pos;
-  int i, line;
+  if (curline >= limit || curline == 0)
+    return 0;
 
-  pos = -1;
-  line = 0;
-  do
+  for (line = curline - 1; line >= 0; line--)
   {
-    for (i = pos + 1; ((i - pos + 1) < MAXLINE - 1) && (s[i] != '\0' && s[i] != '\n'); i++)
-      lines[line][i - (pos + 1)] = s[i];
-    lines[line][i] = '\0';
-
-    line++;
-  } while ((line < mlns) && (pos = strindex(s, "\n", pos + 1)) >= 0);
-
-  return line;
-}
-
-int strindex(const char s[], const char t[], unsigned int beg)
-{
-  int pos, slen;
-  int i, j;
-
-  for (slen = 0; s[slen] != '\0'; slen++)
-    ;
-  for (i = beg, pos = -1; pos == -1 && i < slen; i++)
-  {
-    for (j = 0; t[j] != '\0' && s[i + j] == t[j]; j++)
+    for (i = 0; (lines[curline][i] != '\0') && (lines[curline][i] == lines[line][i]); i++)
       ;
-    if (j > 0 && t[j] == '\0')
-      pos = i;
+    // Si es el fin de linea de curline asi como de la linea actual, quiere decir que son iguales y por lo tanto si esta duplicada la linea.
+    if (i > 0 && lines[curline][i] == '\0' && lines[line][i] == '\0')
+      return 1;
   }
-  return pos;
-}
-
-int strlength(const char s[])
-{
-  int i;
-  for (i = 0; s[i] != '\0'; i++)
-    ;
-  return i;
+  return 0;
 }
