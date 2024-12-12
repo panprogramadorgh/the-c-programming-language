46a3f98e336cd82845bf203799162528e62a629f
panprogramadorgh <barreroalvaro2007@gmail.com>
2024-12-12 16:41:09 +0100

    Funcion usage en brep

--- capitulo-5/brep.c
+++ capitulo-5/brep.c
@@ -16,15 +16,18 @@ int getln(char *line, int max);
 // Retorna != 0 si b se encuentra en a
 int strindex(char *a, char *b);
 
+// Imprime instrucciones de uso
+void prtusage(const char *first_arg);
+
 int main(int argc, char *argv[])
 {
   char line[MAXLN], before[MAXLN], after[MAXLN];
   int line_len, word_len, word_index;
 
-  if (argc < 2)
+  if (argc != 2)
   {
+    prtusage(argv[0]);
     errno = EINVAL;
-    perror("error: first argument was not specified");
     return EXIT_FAILURE;
   }
   word_len = strlen(argv[1]);
@@ -32,8 +35,8 @@ int main(int argc, char *argv[])
   while ((line_len = getln(line, MAXLN)) > 0)
     if ((word_index = strindex(line, argv[1])) != -1)
     {
-      memset(before, 0, MAXLN);
-      memset(after, 0, MAXLN);
+      memset(before, '\0', MAXLN);
+      memset(after, '\0', MAXLN);
       strncpy(before, line, word_index);
       strcpy(after, line + word_index + word_len);
       printf("%s", before);
@@ -62,4 +65,12 @@ int strindex(char *a, char *b)
     if (strncmp((a + i), b, b_len) == 0)
       return i;
   return -1;
+}
+
+void prtusage(const char *first_arg)
+{
+  printf("BREP - Blue GREP\n");
+  printf("Allows you to find patterns in the text. To close program input stream, pres CTRL + C.\n");
+  printf("\t%s <pattern>\n", first_arg);
+  return;
 }
